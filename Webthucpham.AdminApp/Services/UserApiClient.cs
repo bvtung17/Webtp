5d3d7a4fe3ef29939ae100f5f0bbc39f1cefebb3
Tùng Bùi <no.problem.161120@gmail.com>
2021-08-16 21:57:34 +0700

    xong 24

---
+++ Webthucpham.AdminApp/Services/UserApiClient.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Webthucpham.ViewModels.System.Users;
+
+namespace Webthucpham.AdminApp.Services
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public UserApiClient(IHttpClientFactory httpClientFactory) // tạo đối tượng 
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://localhost:5001");
+            var response = await client.PostAsync("/api/users/authenticate", httpContent);
+            var token = await response.Content.ReadAsStringAsync();
+            return token;
+        } 
+
+    }
+}
