450acc9262508957636bcb3289617a84f6c0f537
Tùng Bùi <no.problem.161120@gmail.com>
2021-09-17 02:35:34 +0700

    xong

--- Webthucpham/Controllers/HomeController.cs
+++ Webthucpham/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using LazZiya.ExpressLocalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Webthucpham.Models;
@@ -12,14 +15,16 @@ namespace Webthucpham.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ISharedCultureLocalizer _loc;
+        public HomeController(ILogger<HomeController> logger, ISharedCultureLocalizer loc)
         {
             _logger = logger;
+            _loc = loc;
         }
 
         public IActionResult Index()
         {
+            var msg = _loc.GetLocalizedString("Vietnamese"); 
             return View();
         }
 
@@ -33,5 +38,17 @@ namespace Webthucpham.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SetCultureCookie(string cltr, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cltr)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+            return LocalRedirect(returnUrl);
+        }
+
     }
 }
