50135bb6dc88015e378c4f530c6b2ecccefa0cac
Tùng Bùi <no.problem.161120@gmail.com>
2021-09-24 15:19:31 +0700

    xong

--- Webthucpham/Controllers/CartController.cs
+++ Webthucpham/Controllers/CartController.cs
@@ -13,58 +13,82 @@ namespace Webthucpham.Controllers
 {
     public class CartController : Controller
     {
-      
-            private readonly IProductApiClient _productApiClient;
 
-            public CartController(IProductApiClient productApiClient)
+        private readonly IProductApiClient _productApiClient;
+
+        public CartController(IProductApiClient productApiClient)
+        {
+            _productApiClient = productApiClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetListItems()
+        {
+            var session = HttpContext.Session.GetString(SystemConstants.CartSession);
+            List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
+            if (session != null)
+                currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
+            return Ok(currentCart);
+        }
+
+        public async Task<IActionResult> AddToCart(int id, string languageId)
+        {
+            var product = await _productApiClient.GetById(id, languageId);
+
+            var session = HttpContext.Session.GetString(SystemConstants.CartSession);
+            List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
+            if (session != null)
+                currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
+
+            int quantity = 1;
+            if (currentCart.Any(x => x.ProductId == id))
             {
-                _productApiClient = productApiClient;
+                quantity = currentCart.First(x => x.ProductId == id).Quantity + 1;
             }
 
-            public IActionResult Index()
+            var cartItem = new CartItemViewModel()
             {
-                return View();
-            }
+                ProductId = id,
+                Description = product.Description,
+                Image = product.ThumbnailImage,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = quantity
+            };
 
-            [HttpGet]
-            public IActionResult GetListItems()
+            currentCart.Add(cartItem);
+
+            HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
+            return Ok(currentCart);
+        }
+        public IActionResult UpdateCart(int id, int quantity)
+        {
+            var session = HttpContext.Session.GetString(SystemConstants.CartSession);
+            List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
+            if (session != null)
+                currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
+
+            foreach (var item in currentCart)
             {
-                var session = HttpContext.Session.GetString(SystemConstants.CartSession);
-                List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
-                if (session != null)
-                    currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
-                return Ok(currentCart);
-            }
-
-            public async Task<IActionResult> AddToCart(int id, string languageId)
-            {
-                var product = await _productApiClient.GetById(id, languageId);
-
-                var session = HttpContext.Session.GetString(SystemConstants.CartSession);
-                List<CartItemViewModel> currentCart = new List<CartItemViewModel>();
-                if (session != null)
-                    currentCart = JsonConvert.DeserializeObject<List<CartItemViewModel>>(session);
-
-                int quantity = 1;
-                if (currentCart.Any(x => x.ProductId == id))
+                if (item.ProductId == id)
                 {
-                    quantity = currentCart.First(x => x.ProductId == id).Quantity + 1;
+                    if (quantity == 0)
+                    {
+                        currentCart.Remove(item);
+                        break;
+                    }
+                    item.Quantity = quantity;
                 }
-
-                var cartItem = new CartItemViewModel()
-                {
-                    ProductId = id,
-                    Description = product.Description,
-                    Image = product.ThumbnailImage,
-                    Name = product.Name,
-                    Price = product.Price,
-                    Quantity = quantity
-                };
-
-                currentCart.Add(cartItem);
-
-                HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
-                return Ok(currentCart);
             }
+
+            HttpContext.Session.SetString(SystemConstants.CartSession, JsonConvert.SerializeObject(currentCart));
+            return Ok(currentCart);
         }
     }
+} 
+
