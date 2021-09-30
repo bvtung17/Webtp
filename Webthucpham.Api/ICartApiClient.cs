9925fb3c5cb96e54631609ddad00f0fe70133503
Tùng Bùi <no.problem.161120@gmail.com>
2021-09-30 17:10:24 +0700

    api_cart

---
+++ Webthucpham.Api/ICartApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Webthucpham.ViewModels.Catalog.Carts;
+using Webthucpham.ViewModels.Common;
+
+namespace Webthucpham.Api
+{
+    public interface ICartApiClient
+    {
+        Task<ClientCartViewModel> AddToCart(ClientCartViewModel request);
+        Task<ClientCartViewModel> UpdateCart(ClientCartViewModel request);
+        Task<ApiResult<ClientCartViewModel>> GetCart(Guid id);
+        Task<ApiResult<ClientCartViewModel>> RemoveProduct(DeleteProductInCartRequest request);
+    }
+}
